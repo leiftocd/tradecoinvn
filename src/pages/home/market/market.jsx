d97f8e7d614f943662a149cbb0061bd59b031dcf
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-01 23:01:08 +0700

    fix

--- src/pages/home/market/market.jsx
+++ src/pages/home/market/market.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { Card } from "../../../components/Card/Card";
 import { Slide } from "../../../components/Slide/Slide";
 import "swiper/css/free-mode";
 import { FreeMode, Autoplay } from "swiper/modules";
 import "./market.css";
 function Market() {
+  const images = [
+    "/hashkey.png",
+    "/okx.png",
+    "/mexc.png",
+    "/byBit.png",
+    "/binance.png",
+    "/bitget.png",
+    "/bingX.png",
 
+  ];
+  const titles = ["hashkey", "okx", "mexc", "byBit", "binance", "bitget", "bingX"];
+
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
-    
-    <section>
+    <section id="market">
 
         {/* MARKET SECTION: START */}
         <div className="w-full h-full mx-[auto] flex py-[3rem] justify-center max-w-[108rem] px-[3rem] market-res">
@@ -111,105 +123,35 @@ function Market() {
                 LÀM QUEN THỊ TRƯỜNG BẰNG CÁCH TẠO TÀI KHOẢN SÀN GIAO DỊCH
               </h2>
 
-              <div className="relative w-full p-[0_15px_30px]">
+              <div className="relative w-full p-[0_15px_30px] mket">
                 <img
                   className="absolute aspect-square max-w-[18%] -top-[2rem] -left-[2.5rem] z-10 left-img"
                   src="/smoker.png"
                 />
                 <Slide.Root
                   slidesPerView={3}
-                  spaceBetween={20}
+                  spaceBetween={10}
                   freeMode={true}
                   modules={[FreeMode, Autoplay]}
                   loop={true}
-                  auto={true}
                   autoplay={{ delay: 3000, disableOnInteraction: false }}
+                  onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+                  
                   className="slide-content"
-                  onSlideChange={(swiper) => {
-                    const slides = document.querySelectorAll('.swiper-slide');
-                    slides.forEach((slide, index) => {
-                      slide.style.transform = index === swiper.realIndex ? 'scale(1.1)' : 'scale(1)';
-                      slide.style.transition = 'transform 0.5s ease-in-out';
-                    });
-                  }}
-                  onInit={(swiper) => {
-                    // Use swiper to get the initial active slide
-                    const slides = document.querySelectorAll('.swiper-slide');
-                    slides[swiper.realIndex].style.transform = 'scale(1.1)'; // Scale the active slide
-                    slides[swiper.realIndex].style.transition = 'transform 0.5s ease-in-out';
-                  }}
                 >
-                    
-                    <Slide.Item >
-                      <div className="p-[1rem] mb-card">
-                        <Card
-                          img="/byBit.png"
-                          title="Đăng kí tài khoản BYBIT"
-                          description="Mã giới thiệu: 19986"
-                          href="https://go.tradecoinvn.com/bybit"
-                        />
-                      </div>
-                    </Slide.Item>
-                    <Slide.Item >
-                      <div className="p-[1rem] mb-card">
-                        <Card
-                          img="/binance.png"
-                          title="Đăng kí tài khoản Binance"
-                          description="Mã giới thiệu: 19986"
-                          href="https://go.tradecoinvn.com/Binance"
-                        />
-                      </div>
-                    </Slide.Item>
-                    <Slide.Item >
-                      <div className="p-[1rem] mb-card">
-                        <Card
-                          img="/bitget.png"
-                          title="Đăng kí tài khoản BitGet"
-                          description="Mã giới thiệu: 19986"
-                          href="https://go.tradecoinvn.com/bitget"
-                        />
-                      </div>
-                    </Slide.Item>
-                    <Slide.Item >
-                      <div className="p-[1rem] mb-card">
-                        <Card
-                          img="/okx.png"
-                          title="Đăng kí tài khoản OkX"
-                          description="Mã giới thiệu: 19986"
-                          href="https://go.tradecoinvn.com/okx"
-                        />
-                      </div>
-                    </Slide.Item>
-                    <Slide.Item >
-                      <div className="p-[1rem] mb-card">
-                        <Card
-                          img="/mexc.png"
-                          title="Đăng kí tài khoản Mexc"
-                          description="Mã giới thiệu: 19986"
-                          href="https://go.tradecoinvn.com/mexc"
-                        />
-                      </div>
-                    </Slide.Item>
-                    <Slide.Item >
-                      <div className="p-[1rem] mb-card ">
-                        <Card
-                          img="/bingX.png"
-                          title="Đăng kí tài khoản BingX"
-                          description="Mã giới thiệu: 19986"
-                          href="https://go.tradecoinvn.com/bingx"
-                        />
-                      </div>
-                    </Slide.Item>
-                    <Slide.Item >
-                      <div className="p-[1rem] mb-card">
-                        <Card
-                          img="/hashkey.png"
-                          title="Đăng kí tài khoản Hashkey"
-                          description="Mã giới thiệu: 19986"
-                          href="https://go.tradecoinvn.com/hashkey"
-                        />
-                      </div>
-                    </Slide.Item>
+                    {titles.map((title, index) => (
+                      <Slide.Item key={index}>
+                        <div className="p-[1rem]">
+                          <Card
+                            href={`https://go.tradecoinvn.com/${title}`}
+                            img={images[index] || "/default.png"}
+                            title={`Đăng kí tài khoản ${title.charAt(0).toUpperCase() + title.slice(1)}`}
+                            description="Mã giới thiệu: 19986"
+                            className={index === activeIndex && "active"}
+                          />
+                        </div>
+                      </Slide.Item>
+                    ))}
                 </Slide.Root>
                 <div className="w-full flex justify-end">
                   <Card className='max-w-[12.5rem] h-[12.5rem]' imgOnly img="/build.png" />
