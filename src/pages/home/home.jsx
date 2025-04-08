c15ce7182bf0f05aec9ae5effec26c8a7e66c6bf
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-09 00:30:43 +0700

    fix

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -12,12 +12,12 @@ function Home() {
       <SEO 
         title="Trang chủ - TRADECOINVN"
         description="Tham gia TRADECOINVN - Cộng đồng Crypto hàng đầu Việt Nam để nhận nhiều lợi ích và cơ hội đầu tư."
-        image="https://images.unsplash.com/photo-1726137569966-a7354383e2ae?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        image="{`${window.location.origin}/logotitle.png`}"
         url={window.location.href}
         //op graph
         ogTitle="Trang chủ - TRADECOINVN"
         ogDescription="Tham gia TRADECOINVN - Cộng đồng Crypto hàng đầu Việt Nam để nhận nhiều lợi ích và cơ hội đầu tư."
-        ogImage={`${window.location.origin}/logotitle.png`}
+        ogImage="https://images.unsplash.com/photo-1726137569966-a7354383e2ae?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         ogUrl={window.location.href}
         ogType="website"
 
