50d5a417a590278e8b3694289b74ba326bd0314d
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-08 19:00:56 +0700

    update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,9 +3,17 @@ import Benefit from './benefit/benefit.jsx'
 import Market from './market/market.jsx'
 import Partner from './partner/partner.jsx'
 import Footer from './footer/footer.jsx'
+import { Helmet } from 'react-helmet'
+import LogoTitle from '../../../public/logotitle.png'
 function Home() {
   return (
     <>
+        <Helmet>
+            <title>Trang chủ - TRADECOINVN</title>
+            <meta property="og:type" content="website" />
+            <meta property="og:image" content={LogoTitle} />
+            <meta property="og:url" content={LogoTitle}/>
+        </Helmet>
         <Banner />
         <Benefit />
         <Market />
