1be0cbf5cb0b15e8601fa98ace85031d5610c7c4
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-09 00:34:25 +0700

    fix

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,6 @@ import Footer from './footer/footer.jsx';
 import SEO from '../../components/Seo/Seo.jsx';
 
 function Home() {
-
   return (
     <>
       <SEO 
@@ -17,10 +16,9 @@ function Home() {
         //op graph
         ogTitle="Trang chủ - TRADECOINVN"
         ogDescription="Tham gia TRADECOINVN - Cộng đồng Crypto hàng đầu Việt Nam để nhận nhiều lợi ích và cơ hội đầu tư."
-        ogImage="https://images.unsplash.com/photo-1726137569966-a7354383e2ae?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        ogImage="{`${window.location.origin}/logotitle.png`}"
         ogUrl={window.location.href}
         ogType="website"
-
         // Twitter meta tags
         twitterCard="summary_large_image"
         twitterTitle="Trang chủ - TRADECOINVN"
