5b95655c33e880bf793b7600e8876ede4ef4da2a
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-09 12:57:40 +0700

    fix

--- src/components/Seo/Seo.jsx
+++ src/components/Seo/Seo.jsx
@@ -12,19 +12,19 @@ function SEO({ title, description, url, ogTitle, ogDescription, ogImage, ogUrl }
       {/* Standard metadata tags */}
       <title>{pageTitle}</title>
       <meta name="description" content={pageDescription} data-react-helmet="true"/>
-      <meta name="image" content="/logotitle.png" data-react-helmet="true"/>
+      <meta name="image" content="{`${window.location.origin}/logotitle.png`}" data-react-helmet="true"/>
       {/* Open Graph tags */}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={ogTitle || pageTitle} />
       <meta property="og:description" content={ogDescription || pageDescription} />
       <meta property="og:url" content={ogUrl || pageUrl} />
-      <meta property="og:image" content="/logotitle.png" />
-      <meta property="og:image:secure_url" content="/logotitle.png" />
+      <meta property="og:image" content={ogImage || `${window.location.origin}/logotitle.png`} />
+      <meta property="og:image:secure_url" content={ogImage || `${window.location.origin}/logotitle.png`} />
       {/* Open tw tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={ogTitle || pageTitle} />
       <meta name="twitter:description" content={ogDescription || pageDescription} />
-      <meta name="twitter:image" content="/logotitle.png" />
+      <meta name="twitter:image" content={ogImage || `${window.location.origin}/logotitle.png`} />
     </Helmet>
   );
 }
