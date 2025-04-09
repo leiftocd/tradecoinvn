f659ffa2676b529b67bf4d3df9b5ecd6dda882af
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-09 13:29:08 +0700

    index httml

--- src/components/Seo/Seo.jsx
+++ src/components/Seo/Seo.jsx
@@ -1,30 +1,38 @@
 import { Helmet } from 'react-helmet-async';
+
 /* eslint-disable react/prop-types */
-function SEO({ title, description, url, ogTitle, ogDescription, ogImage, ogUrl }) {
+function SEO({ title, description, name, type, url, image, twitterCard, ogTitle, ogDescription, ogImage, ogUrl }) {
   const pageTitle = title || "Default Title";
   const pageDescription = description || "Default description for the page.";
   const pageUrl = url || window.location.href;
+  const pageImage = image || `${window.location.origin}/logotitle.png`;
 
   return (
     <Helmet>
       {/* Character encoding meta tag */}
       <meta charSet="UTF-8" />
+      
       {/* Standard metadata tags */}
-      <title data-react-helmet="true">{pageTitle}</title>
-      <meta name="description" content={pageDescription} data-react-helmet="true"/>
-      <meta name="image" content="{`${window.location.origin}/logotitle.png`}" data-react-helmet="true"/>
+      <title>{pageTitle}</title>
+      <meta name="description" content={pageDescription} />
+      <meta name="image" content={pageImage} />
+
       {/* Open Graph tags */}
-      <meta property="og:type" content="website" data-react-helmet="true"/>
-      <meta property="og:title" content={ogTitle || pageTitle} data-react-helmet="true"/>
-      <meta property="og:description" content={ogDescription || pageDescription} data-react-helmet="true"/>
-      <meta property="og:url" content={ogUrl || pageUrl} data-react-helmet="true"/>
-      <meta property="og:image" content={ogImage || `${window.location.origin}/logotitle.png`} data-react-helmet="true"/>
-      <meta property="og:image:secure_url" content={ogImage || `${window.location.origin}/logotitle.png`} data-react-helmet="true"/>
-      {/* Open tw tags */}
-      <meta name="twitter:card" content="summary_large_image" data-react-helmet="true"/>
-      <meta name="twitter:title" content={ogTitle || pageTitle} data-react-helmet="true"/>
-      <meta name="twitter:description" content={ogDescription || pageDescription} data-react-helmet="true"/>
-      <meta name="twitter:image" content={ogImage || `${window.location.origin}/logotitle.png`} data-react-helmet="true"/>
+      <meta property="og:type" content={type || 'website'} />
+      <meta property="og:title" content={ogTitle || pageTitle} />
+      <meta property="og:description" content={ogDescription || pageDescription} />
+      <meta property="og:url" content={ogUrl || pageUrl} />
+      <meta property="og:image" content={ogImage || pageImage} />
+      
+      {/* Only the og:image:secure_url for HTTPS support */}
+      <meta property="og:image:secure_url" content={ogImage || pageImage} />
+
+      {/* Twitter Card tags */}
+      <meta name="twitter:creator" content={name || '@default'} />
+      <meta name="twitter:card" content={twitterCard || 'summary_large_image'} />
+      <meta name="twitter:title" content={ogTitle || pageTitle} />
+      <meta name="twitter:description" content={ogDescription || pageDescription} />
+      <meta name="twitter:image" content={ogImage || pageImage} />
     </Helmet>
   );
 }
