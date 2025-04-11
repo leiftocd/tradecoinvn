70ddd6319752262b2214a3fb25c2417fed64b7ed
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-11 21:41:27 +0700

    fix .html

--- api/index.js
+++ api/index.js
@@ -7,17 +7,24 @@ const app = express();
 const distPath = path.join(__dirname, '..', 'dist');
 const publicPath = path.join(__dirname, '..', 'public');
 
-app.use(express.static(distPath));
-app.use(express.static(publicPath));
-
 app.get('/:slug', (req, res, next) => {
   const slug = req.params.slug;
-  const filePath = path.join(distPath, `${slug}.html`);
-  if (fs.existsSync(filePath)) {
-    return res.sendFile(filePath);
+
+  const publicHtml = path.join(publicPath, `${slug}.html`);
+  if (fs.existsSync(publicHtml)) {
+    return res.sendFile(publicHtml);
   }
-  return next();
+
+  const distHtml = path.join(distPath, `${slug}.html`);
+  if (fs.existsSync(distHtml)) {
+    return res.sendFile(distHtml);
+  }
+
+  return next(); 
 });
+
+app.use(express.static(distPath));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
