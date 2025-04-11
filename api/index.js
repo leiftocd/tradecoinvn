f3579886acdf1588eced5be32627f7b04d4bbc34
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-11 21:48:43 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -9,28 +9,21 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.get('/:slug', (req, res, next) => {
   const slug = req.params.slug;
-
   const publicHtml = path.join(publicPath, `${slug}.html`);
   const distHtml = path.join(distPath, `${slug}.html`);
 
   if (fs.existsSync(publicHtml)) {
     return res.sendFile(publicHtml);
   }
-
   if (fs.existsSync(distHtml)) {
     return res.sendFile(distHtml);
   }
 
-  return next();
+  return next(); 
 });
 
-app.get('/public/:file', (req, res) => {
-  const filePath = path.join(publicPath, req.params.file);
-  if (fs.existsSync(filePath)) {
-    return res.sendFile(filePath);
-  }
-  return res.status(404).send('File not found');
-});
+app.use(express.static(distPath));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
