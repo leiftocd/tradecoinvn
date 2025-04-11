8091048b75dd1ba24cb8aee4bb5b2868ed9465fa
Thanh Luan <luannguyengl2001@gmail.com>
2025-04-11 19:32:13 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -4,14 +4,13 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-
 const distPath = path.resolve(__dirname, '../dist');
+
 app.use(express.static(distPath));
 
 app.get('/:slug(Binance|BingX|Hashkey|OKX|MEXC|BYBIT|BitGet|link-telegram-channel|link-telegram-support)', (req, res) => {
   const slug = req.params.slug;
   const filePath = path.join(distPath, `${slug}.html`);
-  console.log('Looking for file:', filePath);
 
   if (fs.existsSync(filePath)) {
     return res.sendFile(filePath);
